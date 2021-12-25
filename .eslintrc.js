841206ab2cc63b650735d84ee0c9e858385dc608
Your Name <pacg1991@gmail.com>
2021-12-25 12:19:02 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  ignorePatterns: ['jsconfig.js', '**/vendor/*.js'],
+  // add your custom rules here
+  rules: {}
+}
